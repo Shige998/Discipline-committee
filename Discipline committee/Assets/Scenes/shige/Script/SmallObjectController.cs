a84cc01b3e8511189d9b377996fa495e4a177fdc
しげぇぇぇぇぇ <kazori4ge@icloud.com>
2026-01-16 15:33:32 +0900

    【バグの修正】

--- Discipline committee/Assets/Scenes/shige/Script/SmallObjectController.cs
+++ Discipline committee/Assets/Scenes/shige/Script/SmallObjectController.cs
@@ -5,6 +5,12 @@ public class SmallObjectController : MonoBehaviour
     [SerializeField] MeshFilter meshFilter;
     [SerializeField] MeshRenderer meshRenderer;
 
+    private string objectName;
+        void Awake()
+    {
+        objectName = gameObject.name;
+    }
+
     public void Apply(SmallObjectData data)
     {
         meshFilter.mesh = data.mesh;
--- Discipline committee/Assets/Scenes/shige/Script/SmallObjectData.cs
+++ Discipline committee/Assets/Scenes/shige/Script/SmallObjectData.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "SmallObject/Data")]
 public class  SmallObjectData : ScriptableObject 
 {
+
+    public string displayName;
     public Mesh mesh;
     public Material material;
     public Vector3 scale = Vector3.one;
