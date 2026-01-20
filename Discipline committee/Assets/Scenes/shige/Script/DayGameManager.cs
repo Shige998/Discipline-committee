17a6c7448040c47ad20918e1e76023c076498552
しげぇぇぇぇぇ <kazori4ge@icloud.com>
2026-01-20 13:15:50 +0900

    【ジャッジ等の修正】

--- Discipline committee/Assets/Scenes/shige/Script/DayGameManager.cs
+++ Discipline committee/Assets/Scenes/shige/Script/DayGameManager.cs
@@ -5,15 +5,55 @@ public class DayGameManager : MonoBehaviour
     public DayData dayData;
     public Transform[] stands;
     public GameObject smallObjectPrefab;
+
+    private int phaseIndex = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartPhaseA();
     }
 
     // Update is called once per frame
-    void Update()
+    void StartPhaseA()
     {
-        
+        ApplyRandomPattern(dayData.phaseA);
+        phaseIndex = 0;
+    }
+    void StartPhaseB()
+    {
+        ApplyRandomPattern(dayData.phaseB);
+        phaseIndex = 1;
+    }
+
+    void ApplyRandomPattern(PhasePattern phase)
+    {
+        var pattern = phase.patterns[Random.Range(0,phase.patterns.Length)];
+
+        foreach (var entry in pattern.placements)
+        {
+            Transform mount = stands[entry.standIndex].GetChild(entry.mountPointIndex);
+
+
+            var obj = Instantiate(smallObjectPrefab,
+                mount.position,
+                mount.rotation
+                );
+
+            obj.GetComponent<SmallObjectController>()
+                .Apply(entry.data);
+        }
+    }
+
+    public void FinishPhase()
+    {
+        foreach (var obj in FindObjectsOfType<SmallObjectController>())
+        {
+            Destroy(obj.gameObject);
+        }
+
+        if (phaseIndex == 0)
+            StartPhaseB();
+        else
+            Debug.Log("Day Finished");
     }
 }
